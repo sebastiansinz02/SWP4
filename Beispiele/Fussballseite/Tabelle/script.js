6bfd21e7ac353f0cfe157743a6e976e51dafbaf0
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-02-03 11:10:10 +0100

    Teams

--- Beispiele/Fussballseite/Tabelle/script.js
+++ Beispiele/Fussballseite/Tabelle/script.js
@@ -1,19 +1,21 @@
+
+loadTabelle();
+
 function loadTabelle() {
     fetch('https://www.openligadb.de/api/getbltable/bl1/2021')
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            let html = " ";
+            let html = "";
             data.forEach(element => {
                 html += "<li>" + element.TeamName + "</li>";
+
             });
-            document.getElementById("tabelle").innerHTML = html;
-            console.log(html);
+            document.getElementById("table").innerHTML = html;
+
         })
         .catch(function(err) {
             console.log(err);
-        })
+        });
 }
-
-loadTabelle()
