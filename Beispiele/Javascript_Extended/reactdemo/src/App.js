27c01769ab9a222ec18d85bcda57d8ca9037a2f3
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-02-24 11:09:08 +0100

    03 Die erste React Komponente

---
+++ Beispiele/Javascript_Extended/reactdemo/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SupperButton from "./components/SupperButton";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <SupperButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ Beispiele/Javascript_Extended/reactdemo/src/components/SupperButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SupperButton extends Component {
+  render() {
+    return (
+      <div>
+        <button>Button</button>
+      </div>
+    );
+  }
+}
