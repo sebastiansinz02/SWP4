a6b425dbf3f86371cd614f889bdbb1f072d17d86
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-02-24 10:29:59 +0100

    image-size

--- Beispiele/Javascript_Extended/nvmtest/script.js
+++ Beispiele/Javascript_Extended/nvmtest/script.js
@@ -1,11 +1,6 @@
-fs = require("fs");
-fs.readFile(
-  "D:\\HTL\\4cWI\\SWP\\Beispiele\\Javascript_Extended\\02\\script.js",
-  "utf8",
-  function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(data);
-  }
+const sizeOf = require("image-size");
+
+const dimensions = sizeOf(
+  "D:\\HTL\\4cWI\\SWP\\Beispiele\\Fussballseite\\Nachwuchs\\U9.jpg"
 );
+console.log(dimensions.width, dimensions.height);
