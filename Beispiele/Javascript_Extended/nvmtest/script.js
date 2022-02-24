6a2be0b24bf220b145c39ffa1e40d5bf608b0dcf
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-02-24 10:25:46 +0100

    fs read

--- Beispiele/Javascript_Extended/nvmtest/script.js
+++ Beispiele/Javascript_Extended/nvmtest/script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+fs = require("fs");
+fs.readFile(
+  "D:\\HTL\\4cWI\\SWP\\Beispiele\\Javascript_Extended\\02\\script.js",
+  "utf8",
+  function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  }
+);
