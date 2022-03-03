5f6c1b308d7fde48a4aa14306756ed9c4b506c01
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-03-03 10:41:20 +0100

    Card

---
+++ Beispiele/Javascript_Extended/reactdemo/src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import styles from "./Card.module.css";
+
+export default class List extends Component {
+  render() {
+    return (
+      <div>
+        <p></p>
+      </div>
+    );
+  }
+}
