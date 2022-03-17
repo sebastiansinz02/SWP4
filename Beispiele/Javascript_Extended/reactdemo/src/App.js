aed08372c9e9f2e639df1c11959cabb2f584798f
sebastian_sinz02 <sebastian.sinz@outlook.com>
2022-03-17 11:08:38 +0100

    hallo

--- Beispiele/Javascript_Extended/reactdemo/src/App.js
+++ Beispiele/Javascript_Extended/reactdemo/src/App.js
@@ -5,6 +5,7 @@ import CounterFunc from "./components/CounterFunc";
 export default function App() {
   return (
     <div>
+      <h1>hallo</h1>
       Functional Component
       <Counter />
       <CounterFunc />
